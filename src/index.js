60607e2bd3ec374466510ce02687d9c44ba5e5b8
unknown <2fixelectronics777@gmail.com>
2022-07-02 15:10:58 -0500

    add finish mod

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 // Get the environment variables
-require('dotenv').config()
-
+require('dotenv').config();
 // Require needed node modules
 //const express = require('express')
-import express from 'express'
-
+const express_1 = __importDefault(require("express"));
 // Initialize your application by calling the function returned by the express module
-const app = express()
-
+const app = (0, express_1.default)();
 // Declare routes that people can visit on the application
-
 // White Page
-app.get('/', function (req: express.Request, res: express.Response):void {
+app.get('/', function (req, res) {
     res.send(`
         <body style="margin: 0;">
             <div style="border: 1px solid black; height: 10vh; background-color: white;">
@@ -19,15 +20,12 @@ app.get('/', function (req: express.Request, res: express.Response):void {
             </div>
             <h1>White Page</h1>
         </body>
-    `)
-})
-
-
+    `);
+});
 // Color Page
 app.get('/:color', function (req, res) {
-    let myColor: unknown = req.params.color
-    if (typeof(myColor) === 'string'){
-
+    let myColor = req.params.color;
+    if (typeof (myColor) === 'string') {
         res.send(`
         <body style="margin: 0;">
             <div style="border: 1px solid black; height: 10vh; background-color: ${myColor};">
@@ -35,13 +33,8 @@ app.get('/:color', function (req, res) {
             </div>
             <h1 style="color: ${myColor};">${myColor.charAt(0).toUpperCase() + myColor.slice(1)} Page</h1>
         </body>
-    `)
-
-
+    `);
     }
-    
-})
-
-
+});
 // Listen to a port number defined by a local environment variable
-app.listen(process.env.PORT)
+app.listen(process.env.PORT);
